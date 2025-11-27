96869eeef574bf668d091a41645e0538f63ad4ba
Longnhh37 <longnhh03@gmail.com>
2025-11-27 15:58:14 +0000

    [LeetCode Sync] Runtime - 238 ms (75.52%), Memory - 0.0B (100.00%)

---
+++ leetcode/2087-confirmation-rate/solution.sql
@@ -0,0 +1,7 @@
+
+select 
+    s.user_id,
+    round(count(case when action = 'confirmed' then 1 end)*1.0/count(*), 2) as confirmation_rate
+from signups s
+left join confirmations c on s.user_id = c.user_id
+group by s.user_id
