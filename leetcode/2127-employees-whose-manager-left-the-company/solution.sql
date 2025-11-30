b9dac87bb79c65b7166ca71a8f09b470decb43ea
Longnhh37 <longnhh03@gmail.com>
2025-11-30 00:39:38 +0000

    [LeetCode Sync] Runtime - 193 ms (92.54%), Memory - 0.0B (100.00%)

---
+++ leetcode/2127-employees-whose-manager-left-the-company/solution.sql
@@ -0,0 +1,6 @@
+-- Write your PostgreSQL query statement below
+select distinct employee_id
+from employees
+where manager_id not in (select employee_id from employees)
+    and salary <30000
+order by 1
