89ce4af464eecad52bbb7b55d481e9ca86a369c0
Longnhh37 <longnhh03@gmail.com>
2026-01-11 05:37:27 +0000

    [LeetCode Sync] Runtime - 270 ms (97.27%), Memory - 67.9 MB (43.64%)

---
+++ leetcode/0197-rising-temperature/solution.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def rising_temperature(weather: pd.DataFrame) -> pd.DataFrame:
+    weather = weather.sort_values('recordDate')
+
+    weather['prev_date'] = weather['recordDate'].shift(1)
+    weather['prev_temp'] = weather['temperature'].shift(1)
+
+    result = weather.loc[
+        (weather['temperature'] > weather['prev_temp'])
+        & ((weather['recordDate'] - weather['prev_date']).dt.days == 1),
+        ['id']
+    ]
+
+    return result
