bbf5d618820e069743c028ff2530ee438d45e7f9
Longnhh37 <longnhh03@gmail.com>
2026-04-13 09:21:02 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.4 MB (66.93%)

--- leetcode/0125-valid-palindrome/solution.rs
+++ leetcode/0125-valid-palindrome/solution.rs
@@ -1,13 +1,14 @@
 impl Solution {
     pub fn is_palindrome(s: String) -> bool {
         let bytes = s.as_bytes();
-    
-        if bytes.is_empty() {
+        let len = bytes.len();
+
+        if len == 0 {
             return true;
         }
 
         let mut l = 0;
-        let mut r = bytes.len() - 1;
+        let mut r = len - 1;
 
         while l < r {
             while l < r && !bytes[l].is_ascii_alphanumeric() {
@@ -17,14 +18,12 @@ impl Solution {
                 r -= 1;
             }
 
-            if l < r { 
-                if bytes[l].to_ascii_lowercase() != bytes[r].to_ascii_lowercase() {
-                    return false;
-                }
-
-                l += 1;
-                r -= 1;
+            if !bytes[l].eq_ignore_ascii_case(&bytes[r]){
+                return false;
             }
+
+            l += 1;
+            r = r.saturating_sub(1);
         }
 
         true
