39a7a6f994b13e3ddf3d3447a1b58e7b47910853
Longnhh37 <longnhh03@gmail.com>
2026-04-13 08:57:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.9 MB (98.41%)

--- leetcode/0121-best-time-to-buy-and-sell-stock/solution.rs
+++ leetcode/0121-best-time-to-buy-and-sell-stock/solution.rs
@@ -1,15 +1,14 @@
 impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
         let mut min_price = i32::MAX;
-        let mut max_profit: i32 = 0;
+        let mut max_profit = 0;
 
-        for price in prices {
-            min_price = min_price.min(price);
-            max_profit = max_profit.max(price - min_price);
+        for &p in prices.iter() {
+            min_price = min_price.min(p);
+            max_profit = max_profit.max(p - min_price);
         }
 
         max_profit
     }
 }
-        
-        
+
