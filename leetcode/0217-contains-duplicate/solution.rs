3df82d2185a0910641284b1cd929714a23eec1d4
Longnhh37 <longnhh03@gmail.com>
2026-04-22 23:35:21 +0000

    [LeetCode Sync] Runtime - 3 ms (93.90%), Memory - 3.8 MB (59.14%)

---
+++ leetcode/0217-contains-duplicate/solution.rs
@@ -0,0 +1,9 @@
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+        let mut seen = HashSet::new();
+
+        nums.into_iter().any(|n| !seen.insert(n))
+    }
+}
