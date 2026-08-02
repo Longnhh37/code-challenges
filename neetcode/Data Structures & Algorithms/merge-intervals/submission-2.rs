ad7314ba4f04bf2578d6f707235f7a7836c123f2
Longnhh37 <longnhh03@gmail.com>
2026-08-02 12:10:20 +0700

    Add: merge-intervals - submission-2

---
+++ neetcode/Data Structures & Algorithms/merge-intervals/submission-2.rs
@@ -0,0 +1,20 @@
+impl Solution {
+    pub fn merge(mut intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        intervals.sort_unstable_by_key(|k| (k[0], k[1]));
+
+        let mut it = intervals.into_iter();
+        let mut cur = it.next().unwrap();
+        let mut res = Vec::with_capacity(it.size_hint().0);
+
+        for next in it {
+            if next[0] <= cur[1] {
+                cur[1] = cur[1].max(next[1]);
+            } else {
+                res.push(std::mem::replace(&mut cur, next));
+            }
+        }
+
+        res.push(cur);
+        res
+    }
+}
