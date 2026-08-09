04ab72322c07b36062beabe3ea49dda5abfed0c1
Longnhh37 <longnhh03@gmail.com>
2026-08-09 13:13:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2 MB (100.00%)

--- leetcode/0874-backspace-string-compare/solution.rs
+++ leetcode/0874-backspace-string-compare/solution.rs
@@ -1,45 +1,16 @@
 impl Solution {
-    fn next_valid(s: &[u8], mut i: Option<usize>)
-        -> (Option<usize>, Option<u8>)
-    {
-        let mut skip = 0;
-
-        while let Some(p) = i {
-            if s[p] == b'#' {
-                skip += 1;
-            } else if skip > 0 {
-                skip -= 1;
-            } else {
-                return (p.checked_sub(1), Some(s[p]));
-            }
-
-            i = p.checked_sub(1);
-        }
-
-        (None, None)
+    pub fn backspace_compare(s: String, t: String) -> bool {
+        Self::helper(&s) == Self::helper(&t)
     }
 
-    pub fn backspace_compare(s: String, t: String) -> bool {
-        let s = s.as_bytes();
-        let t = t.as_bytes();
-
-        let mut i = s.len().checked_sub(1);
-        let mut j = t.len().checked_sub(1);
-
-        loop {
-            let (ni, a) = Self::next_valid(s, i);
-            let (nj, b) = Self::next_valid(t, j);
-
-            if a != b {
-                return false;
+    fn helper(s: &str) -> Vec<u8> {
+        let mut res = Vec::new();
+        for b in s.bytes() {
+            match b {
+                b'#' => { res.pop(); }
+                b => res.push(b),
             }
-
-            if a.is_none() {
-                return true;
-            }
-
-            i = ni;
-            j = nj;
         }
+        res
     }
 }
