5818cd6003183597c31f5aab08784ba9a62d5e13
Longnhh37 <longnhh03@gmail.com>
2026-08-09 18:49:23 +0800

    Add: bitwise-and-of-numbers-range - submission-2

---
+++ neetcode/Data Structures & Algorithms/bitwise-and-of-numbers-range/submission-2.rs
@@ -0,0 +1,8 @@
+impl Solution {
+    pub fn range_bitwise_and(left: i32, right: i32) -> i32 {
+        (left + 1..=right)
+        .fold(left, |mut acc, x| {
+            acc & x
+        })
+    }
+}
