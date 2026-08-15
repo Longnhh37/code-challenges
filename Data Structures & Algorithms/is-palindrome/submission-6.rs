66756352947b23ac1019427186f7294c6ddfce64
Longnhh37 <longnhh03@gmail.com>
2026-08-15 09:26:07 +0800

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.rs
@@ -0,0 +1,25 @@
+impl Solution {
+    pub fn is_palindrome(s: String) -> bool {
+        let b = s.as_bytes();
+        let (mut l, mut r) = (0, b.len() - 1);
+
+        while l < r {
+            while l < r && !b[l].is_ascii_alphanumeric() {
+                l += 1;
+            }
+            while r > l && !b[r].is_ascii_alphanumeric() {
+                r -= 1;
+            }
+            if l >= r {
+                break;
+            }
+
+            if !b[l].eq_ignore_ascii_case(&b[r]) {
+                return false;
+            }
+            l += 1;
+            r -= 1;
+        }
+        true
+    }
+}
