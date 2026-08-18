f93f9e8f36e364937c34fdc33ae235f9d3305214
Longnhh37 <longnhh03@gmail.com>
2026-08-18 10:08:15 +0800

    Add solution - CodeHub

---
+++ codewars/6-kyu/valid-braces/valid-braces.rs
@@ -0,0 +1,13 @@
+fn valid_braces(s: &str) -> bool {
+    let mut stack = Vec::new();
+    for b in s.bytes() {
+        match b {
+            b'{' | b'[' | b'(' => stack.push(b),
+            b'}' if stack.pop() == Some(b'{') => {},
+            b']' if stack.pop() == Some(b'[') => {},
+            b')' if stack.pop() == Some(b'(') => {},
+            _ => return false,
+        }
+    }
+    stack.is_empty()
+}
