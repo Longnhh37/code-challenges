69cd997118af4456b0dc0c4366b4b6421de46bf9
Longnhh37 <longnhh03@gmail.com>
2026-08-22 22:16:42 +0800

    Remove neetcode dups

--- Data Structures & Algorithms/divide-array-into-equal-pairs/submission-0.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def divideArray(self, nums: List[int]) -> bool:
-        counter = Counter(nums)
-        for (_, cnt) in counter.items():
-            if cnt % 2 == 1:
-                return False
-        return True
-        
