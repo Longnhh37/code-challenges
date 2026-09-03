6f459b4abb5eab589f158c1d438e8f4084448a62
Longnhh37 <longnhh03@gmail.com>
2026-09-03 12:24:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.1 MB (96.53%)

---
+++ leetcode/0094-binary-tree-inorder-traversal/solution.rs
@@ -0,0 +1,35 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        let mut res = Vec::new();
+        Self::dfs(root, &mut res);
+        res
+    }
+
+    fn dfs(root: Option<Rc<RefCell<TreeNode>>>, res: &mut Vec<i32>) {
+        let Some(node) = root else { return; };
+        let n = node.borrow();
+        Self::dfs(n.left.clone(), res);
+        res.push(n.val);
+        Self::dfs(n.right.clone(), res);
+    }
+}
