b0e986285a7d7c19590e5a6d308f35bd252134ff
Longnhh37 <longnhh03@gmail.com>
2026-09-03 13:34:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.1 MB (56.76%)

---
+++ leetcode/0202-happy-number/solution.rs
@@ -0,0 +1,21 @@
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn is_happy(mut n: i32) -> bool {
+        let mut seen = HashSet::new();       
+        loop {
+            let mut sum = 0;
+            while n > 0 {
+                sum += (n % 10).pow(2);
+                n /= 10;
+            }
+            if sum == 1 {
+                return true;
+            }
+            if !seen.insert(sum) {
+                return false;
+            }
+            n = sum;
+        }
+    }
+}
