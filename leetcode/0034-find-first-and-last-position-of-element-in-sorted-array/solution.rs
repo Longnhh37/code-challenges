908fe3b4c1f54f6814b18044d45098aba6e512ca
Longnhh37 <longnhh03@gmail.com>
2026-09-06 07:43:45 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.3 MB (83.33%)

---
+++ leetcode/0034-find-first-and-last-position-of-element-in-sorted-array/solution.rs
@@ -0,0 +1,12 @@
+impl Solution {
+    pub fn search_range(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        if let Ok(pos) = nums.binary_search(&target) {
+            vec![
+                nums.partition_point(|&i| i < target) as i32,
+                nums.partition_point(|&i| i <= target) as i32 - 1
+                ]
+        } else {
+            vec![-1, -1]
+        }
+    }
+}
