60c0e37984d177f9bb041fac3726f53de0622907
Longnhh37 <longnhh03@gmail.com>
2026-09-17 10:07:00 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (94.59%)

--- leetcode/0047-permutations-ii/solution.rs
+++ leetcode/0047-permutations-ii/solution.rs
@@ -1,27 +1,28 @@
 impl Solution {
     pub fn permute_unique(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
         nums.sort_unstable();
-        let n = nums.len();
-        let mut used = vec![false; n];
         let mut res = Vec::new();
-        Self::backtrack(&nums, 0, &mut Vec::new(), &mut used, &mut res);
+        let mut visited = vec![false; nums.len()];
+        Self::backtrack(&nums, &mut visited, &mut Vec::new(), &mut res);
         res
     }
 
-    fn backtrack(nums: &[i32], start: usize, path: &mut Vec<i32>, used: &mut [bool], res: &mut Vec<Vec<i32>>) {
+    fn backtrack(nums: &[i32], visited: &mut [bool], path: &mut Vec<i32>, res: &mut Vec<Vec<i32>>) {
         if path.len() == nums.len() {
             return res.push(path.clone());
         }
-        for i in 0..nums.len() {
-            if used[i] { continue; }
-            if i > 0 && nums[i] == nums[i - 1] && !used[i - 1] {
+        for (i, &n) in nums.iter().enumerate() {
+            if visited[i] {
                 continue;
             }
-            used[i] = true;
-            path.push(nums[i]);
-            Self::backtrack(nums, i + 1, path, used, res);
+            if i > 0 && nums[i] == nums[i - 1] && !visited[i - 1] {
+                continue;
+            }
+            visited[i] = true;
+            path.push(n);
+            Self::backtrack(nums, visited, path, res);
             path.pop();
-            used[i] = false;
+            visited[i] = false;
         }
     }
 }
