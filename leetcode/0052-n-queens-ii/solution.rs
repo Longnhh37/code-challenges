00766fd351b1e2464bd275afd7caf7f3b8bc087d
Longnhh37 <longnhh03@gmail.com>
2026-09-18 01:37:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (42.86%)

--- leetcode/0052-n-queens-ii/solution.rs
+++ leetcode/0052-n-queens-ii/solution.rs
@@ -1,17 +1,16 @@
 impl Solution {
     pub fn total_n_queens(n: i32) -> i32 {
-        let n = n as usize;
         let mut res = 0;
-        Self::dfs(0, n, 0, 0, 0, &mut res);
+        Self::backtrack(0, n, 0, 0, 0, &mut res);
         res
     }
 
-    fn dfs(
-        row: usize,
-        n: usize,
+    fn backtrack(
+        row: i32,
+        n: i32,
         cols: u32,
-        diag: u32,
-        anti_diag: u32,
+        diag1: u32, // row + n - col
+        diag2: u32, // row + col
         res: &mut i32,
     ) {
         if row == n {
@@ -19,13 +18,20 @@ impl Solution {
         }
 
         let full_mask = (1u32 << n) - 1;
-        let occupied = cols | diag | anti_diag;
+        let occupied = cols | diag1 | diag2;
         let mut avail = full_mask & !occupied;
 
         while avail != 0 {
             let bit = avail & avail.wrapping_neg();
             avail -= bit;
-            Self::dfs(row + 1, n , cols | bit, (diag | bit) << 1, (anti_diag | bit) >> 1, res);
+            Self::backtrack(
+                row + 1,
+                n,
+                cols | bit,
+                (diag1 | bit) << 1,
+                (diag2 | bit) >> 1,
+                res
+            );
         }
     }
 }
