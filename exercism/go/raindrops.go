5a61a3fceca91b234f8528a0d077a621290b95fa
Longnhh37 <Longnhh03@gmail.com>
2026-09-25 21:35:05 +0800

    im not sure I like go

---
+++ exercism/go/raindrops.go
@@ -0,0 +1,24 @@
+package raindrops
+
+import (
+	"strconv"
+)
+
+func Convert(n int) string {
+	res := ""
+
+	if n%3 == 0 {
+		res += "Pling"
+	}
+	if n%5 == 0 {
+		res += "Plang"
+	}
+	if n%7 == 0 {
+		res += "Plong"
+	}
+
+	if res == "" {
+		return strconv.Itoa(n)
+	}
+	return res
+}
